85dd27c11451ca172dadbaaea8ba71f0cf4611fe
XiaoHan21 <167948944+XiaoHan21@users.noreply.github.com>
2026-05-12 18:40:20 +0800

    Update s02.merge_humann_tables.sh

--- s02.merge_humann_tables.sh
+++ s02.merge_humann_tables.sh
@@ -34,7 +34,7 @@ echo "========================================="
 
 # Activate the conda environment properly
 echo "Activating conda environment (humann4_mpa4.1.1)..."
-source ~/xiaohan/Software/miniconda3/etc/profile.d/conda.sh
+source ~/xiaohan/Software/miniconda3/bin/activate
 conda activate humann4_mpa4.1.1
 
 # Define the standard HUMAnN output table types
